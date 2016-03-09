91c2362d9739ec26fa9e21447e8c6f362cb47d3b
Kim Dongsoo <kimdongsoo5@gmail.com>
2016-03-09 20:22:06 +0900

    Added file resource to testserver.

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,13 @@
 node 'dongkim.linuxmaster.com' {
 }
 
+node 'vm01.linuxmaster.com' {
+  file {'/tmp/testserver':
+    content => "This is a test server.\n",
+  }
+}
+
 node /^vm[0-9]+\.linuxmaster\.com$/ {
 #  include vim_upgrade
-  include sshd
+#  include sshd
 }
